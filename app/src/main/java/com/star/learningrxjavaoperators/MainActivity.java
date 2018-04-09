d26d7ddb8389f5e1921ac8d81cdd412217ea59d3
crystalDf <chendong333@gmail.com>
2018-04-09 21:45:35 +0800

    Carson_Ho-Blog-20171106-LearningRxJavaOperators

--- app/src/main/java/com/star/learningrxjavaoperators/MainActivity.java
+++ app/src/main/java/com/star/learningrxjavaoperators/MainActivity.java
@@ -4,10 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import java.util.ArrayList;
-
 import io.reactivex.Observable;
-import io.reactivex.functions.BiConsumer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -311,12 +308,44 @@ public class MainActivity extends AppCompatActivity {
 //                    return integer * integer2;
 //                })
 //                .subscribe(integer -> Log.d(TAG, "最终计算的结果是: " + integer));
+//
+//        Observable
+//                .just(1, 2, 3, 4, 5, 6)
+//                .collect(
+//                        ArrayList::new,
+//                        (BiConsumer<ArrayList<Integer>, Integer>) ArrayList::add)
+//                .subscribe(integers -> Log.d(TAG, "本次发送的数据是: " + integers));
+//
+//        Observable
+//                .just(4, 5, 6)
+//                .startWith(0)
+//                .startWithArray(1, 2, 3)
+//                .startWith(Observable.just(7, 8, 9))
+//                .subscribe(new Observer<Integer>() {
+//                    @Override
+//                    public void onSubscribe(Disposable d) {
+//                        Log.d(TAG, "开始采用subscribe连接");
+//                    }
+//
+//                    @Override
+//                    public void onNext(Integer integer) {
+//                        Log.d(TAG, "接收到了事件" + integer);
+//                    }
+//
+//                    @Override
+//                    public void onError(Throwable e) {
+//                        Log.d(TAG, "对Error事件作出响应");
+//                    }
+//
+//                    @Override
+//                    public void onComplete() {
+//                        Log.d(TAG, "对Complete事件作出响应");
+//                    }
+//                });
 
         Observable
-                .just(1, 2, 3, 4, 5, 6)
-                .collect(
-                        ArrayList::new,
-                        (BiConsumer<ArrayList<Integer>, Integer>) ArrayList::add)
-                .subscribe(integers -> Log.d(TAG, "本次发送的数据是: " + integers));
+                .just(1, 2, 3, 4)
+                .count()
+                .subscribe(aLong -> Log.d(TAG, "发送的事件数量 = " + aLong));
     }
 }
